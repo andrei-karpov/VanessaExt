fee4a8159b2d2deedf40469808f0af208b2add6b
Kandrashin Denis <mail@lintest.ru>
2020-11-23 20:40:53 +0300

    Отключена cборка OpenCV для Linux

--- src/ImageFinder.cpp
+++ src/ImageFinder.cpp
@@ -1,4 +1,6 @@
-﻿#include "ImageFinder.h"
+﻿#ifdef _WINDOWS
+
+#include "ImageFinder.h"
 #include "opencv2/imgcodecs.hpp"
 #include "opencv2/imgproc.hpp"
 #include "json.hpp"
@@ -44,3 +46,4 @@ std::string BaseHelper::ImageFinder::find(VH picture, VH fragment, int match_met
     j["bottom"] = matchLoc.y + templ.rows;
     return j.dump();
 }
+#endif//_WINDOWS
--- src/ImageFinder.h
+++ src/ImageFinder.h
@@ -1,5 +1,6 @@
 #ifndef __IMAGEFINDER_H__
 #define __IMAGEFINDER_H__
+#ifdef _WINDOWS
 
 #include "stdafx.h"
 #include "json.hpp"
@@ -10,4 +11,5 @@ public:
 	static std::string find(VH picture, VH fragment, int match_method = 0);
 };
 
+#endif//_WINDOWS
 #endif//__IMAGEFINDER_H__
